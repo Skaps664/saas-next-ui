2580bd51a3ed7780440348ce40c7e3d9a1124c05
Skaps664 <msudaisk664@gmail.com>
2024-10-17 21:07:27 +0500

    footer text color updated

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -4,7 +4,7 @@ import { Card, CardBody } from "@nextui-org/react";
 
 export const RegisterForm = ({ toggleForm }: { toggleForm: () => void }) => {
   return (
-    <Card className="w-full max-w-md bg-gray-900 border border-purple-500 py-12 px-6">
+    <Card className="w-full max-w-md bg-black border border-purple-500 py-12 px-6">
       <CardBody className="p-6">
         <div className="flex flex-col items-center justify-center text-white">
           <h3 className="text-2xl font-bold mb-4">Create a New Account</h3>
@@ -24,7 +24,7 @@ export const RegisterForm = ({ toggleForm }: { toggleForm: () => void }) => {
             className="w-full p-2 mb-4 bg-gray-800 border border-gray-700 rounded text-white focus:outline-none focus:border-purple-500"
           />
           {/* Updated button with a blue border and no background */}
-          <button className="w-full py-3 px-4 border border-yellow-500 text-yellow-500 rounded hover:bg-gray-800 hover:text-white focus:outline-none">
+          <button className="w-full py-3 px-4 border border-purple-500 text-purple-500 rounded hover:bg-gray-800 hover:text-white focus:outline-none">
             Register
           </button>
           <p className="mt-8">
--- components/footer.tsx
+++ components/footer.tsx
@@ -12,34 +12,40 @@ export const Footer = () => {
         {/* Logo Section */}
         <div className="flex items-center space-x-3">
           <img src="/editur_logo.png" alt="Editur Logo" className="w-8 h-8" />
-          <p className="font-bold text-lg">Editur.ai</p>
+          <p className="font-bold text-lg text-white">Editur.ai</p>
         </div>
 
         {/* Footer Links Section */}
-        <div className="flex gap-6 text-base ">
-          <Link href="/pricing" className="hover:text-primary">
+        <div className="flex gap-6 text-base text-white">
+          <Link href="/pricing" className="text-white hover:text-purple-400">
             Pricing
           </Link>
-          <Link href="/features" className="hover:text-primary">
+          <Link href="/features" className="text-white hover:text-purple-400">
             Features
           </Link>
-          <Link href="/about-us" className="hover:text-primary">
+          <Link href="/about-us" className="text-white hover:text-purple-400">
             About Us
           </Link>
-          <Link href="/support" className="hover:text-primary">
+          <Link href="/support" className="text-white hover:text-purple-400">
             Support
           </Link>
-          <Link href="/terms-of-service" className="hover:text-primary">
+          <Link
+            href="/terms-of-service"
+            className="text-white hover:text-purple-400"
+          >
             Terms of Service
           </Link>
-          <Link href="/privacy-policy" className="hover:text-primary">
+          <Link
+            href="/privacy-policy"
+            className="text-white hover:text-purple-400"
+          >
             Privacy Policy
           </Link>
         </div>
       </div>
 
       {/* Bottom Section */}
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center mt-4 text-sm text-default-500 mt-6">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center mt-4 text-sm text-white mt-6">
         {/* Copyright */}
         <p>&copy; {new Date().getFullYear()} Editur.ai. All rights reserved.</p>
 
